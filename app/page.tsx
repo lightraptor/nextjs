746ce895cf02febdee35133df6fc1fcbf185fa86
lightraptor2310 <qle2318@gmail.com>
2024-04-20 00:16:10 +0700

    fix-img-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from 'next/image'; // Import the Image component
+import carot from '@/app/thichcarot.jpg'
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       </div>
       {/* Insert the image here */}
       <div className="image-wrap w-full h-fit flex justify-center items-center">
-        <Image src="thichcarot.jpg" alt="Thich Carot" width={500} height={500} />
+        <Image src={carot} alt="Thich Carot" width={500} height={500} />
       </div>
       <div className="content-wrap flex flex-col gap-3">
         <p className="sm:text-3xl text-xl font-normal tracking-wide text-center">Khiếm khuyết một cách hoàn hảo (hiện tại là với cái web này)</p>
@@ -18,12 +19,12 @@ const Home = () => {
       </div>
       <div className="button-wrap flex sm:flex-row flex-col justify-around sm:my-20 my-10 gap-7">
         <div className="button-left sm:w-1/2n w-full flex justify-center items-center">
-          <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
+          <button className="button-item sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
             <a href="https://form.jotform.com/241073918354458" className=" tracking-wide">Khảo sát Tiếng Việt</a>
           </button>
         </div>
         <div className="button-right sm:w-1/2n w-full flex justify-center items-center">
-          <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
+          <button className="button-item sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
             <a href="https://form.jotform.com/241032420358445" className=" tracking-wide">English Survey</a>
           </button>
         </div>
