d60dfc8c9a2b1f0ae01f527d23cb2a6b2043ddb4
Thích Cà Rốt <kaze2508@outlook.com>
2024-04-20 00:06:32 +0700

    Add picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React from "react";
+import Image from 'next/image'; // Import the Image component
+
 const Home = () => {
   return (
     <div className="m-4 flex flex-col gap-3">
-      {/*       <Image src={img} width={500} height={500} alt="bg" /> */}
       <div className="title-wrap w-full h-fit flex justify-center items-center">
         <p className=" sm:text-8xl text-5xl font-bold my-9 tracking-wider">Wabi-Sabi</p>
+      </div>
+      {/* Insert the image here */}
+      <div className="image-wrap w-full h-fit flex justify-center items-center">
+        <Image src="thichcarot.jpg" alt="Thich Carot" width={500} height={500} />
       </div>
       <div className="content-wrap flex flex-col gap-3">
         <p className="sm:text-3xl text-xl font-normal tracking-wide text-center">Khiếm khuyết một cách hoàn hảo (hiện tại là với cái web này)</p>
@@ -13,12 +18,14 @@ const Home = () => {
       </div>
       <div className="button-wrap flex sm:flex-row flex-col justify-around sm:my-20 my-10 gap-7">
         <div className="button-left sm:w-1/2n w-full flex justify-center items-center">
-        <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300"><a href="https://form.jotform.com/241073918354458" className=" tracking-wide">Khảo sát Tiếng Việt</a></button>
-
+          <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
+            <a href="https://form.jotform.com/241073918354458" className=" tracking-wide">Khảo sát Tiếng Việt</a>
+          </button>
         </div>
         <div className="button-right sm:w-1/2n w-full flex justify-center items-center">
-        <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300"><a href="https://form.jotform.com/241032420358445" className=" tracking-wide">English Survey</a></button>
-
+          <button className="bg-black text-white sm:w-1/2 w-[90%] p-4 text-xl hover:text-2xl hover:sm:text-3xl hover:sm:w-2/3 hover:w-[95%] duration-300">
+            <a href="https://form.jotform.com/241032420358445" className=" tracking-wide">English Survey</a>
+          </button>
         </div>
       </div>
     </div>
